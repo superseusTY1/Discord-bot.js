49bd33e7fa6a15a410fbfe8ce9a6c636a8058881
superseusTY1 <48190779+superseusTY1@users.noreply.github.com>
2019-04-12 11:32:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const Discord = require("discord.js");
 const client = new Discord.Client(); 
 const config = require("./config.json"); 
 
-
 client.on("ready", () => {
-  console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
-  client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`);
-  
-      //STREAMING = TRANSMITINDO
+    console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
+    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`);
+
+          //STREAMING = TRANSMITINDO
     //LISTENING = OUVINDO
     //PLAYING = JOGANDO
     //WATCHING = ASSISTINDO
@@ -16,30 +15,19 @@ client.on("ready", () => {
     
 
     let status = [     
-      {name: `Olá Mundo!`, type: 'playing'}
-      {name: `Olá Mundo!`, type: 'STREAMING', url: 'https://www.twitch.tv/alanzoka'},
-      ];   
-  
-          function setStatus() {
-          let randomStatus = status[Math.floor(Math.random() * status.length)];
-          client.user.setPresence({game: randomStatus});
-      }
+        {name: `Olá Mundo!`, type: 'playing'},
+        {name: `Olá Mundo!`, type: 'STREAMING', url: 'https://www.twitch.tv/alanzoka'},
+        ];   
     
-      setStatus();
-      setInterval(() => setStatus(), 5000);
+            function setStatus() {
+            let randomStatus = status[Math.floor(Math.random() * status.length)];
+            client.user.setPresence({game: randomStatus});
+        }
       
-    });  
-    
-    
-      if(comando === "ping") {
-    const m = await message.channel.send("`API Ping : `, Math.floor(client.ping) + 'ms', `Bot Ping : `, Math.floor(botping) + 'ms' ")
-    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
-  }
-  
-  if(comando === "say") { 
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{});  
-    message.channel.send(sayMessage);
-  }
-    
-    
+        setStatus();
+        setInterval(() => setStatus(), 5000);
+        
+})
+
+
+client.login(config.token)
